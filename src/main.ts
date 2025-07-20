e95bb20bb6c9eb31feaf441536c5e9a487494d9b
Matills <rubiolomatias2001@gmail.com>
2025-07-20 01:45:51 -0300

    config intial

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router/router";
+import pinia from "./store/store";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+
+app.mount("#app");
